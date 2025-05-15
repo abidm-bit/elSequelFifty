4d0ac159323c95c6cb45852b6efcfd1418df3926
abidm-bit <abidmres@gmail.com>
2025-05-15 00:15:02 -0400

    LC584

---
+++ CreateTables/lc584Table.sql
@@ -0,0 +1,6 @@
+-- LC584. Find Customer Referee
+
+create table Customer
+(id INT,
+ name VARCHAR(75),
+ referee_id INT,primary key (id));
---
+++ InsertData/lc584TestData.sql
@@ -0,0 +1,7 @@
+insert into customer
+values(1,'Will',null),
+      (2,'Jane',null),
+      (3,'Alex',2),
+      (4,'Bill',null),
+      (5,'Zack',1),
+      (6,'Mark',2);
