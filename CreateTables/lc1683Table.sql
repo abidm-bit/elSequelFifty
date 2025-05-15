c0285523d010fe109d2d31e882a10f4e588a1175
abidm-bit <abidmres@gmail.com>
2025-05-15 00:50:36 -0400

    1683. Invalid Tweets

---
+++ CreateTables/lc1683Table.sql
@@ -0,0 +1,6 @@
+-- LC1683
+
+create table Tweets
+(tweet_id int,
+content varchar(75),
+primary key (tweet_id));
---
+++ InsertData/lc1683TestData.sql
@@ -0,0 +1,4 @@
+insert into tweets
+values
+    (1,'Let us Code'),
+    (2,'More than fifteen chars are here');
