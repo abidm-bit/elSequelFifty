9ffd50890f22c626f3da05bade4cd66f88067407
abidm-bit <abidmres@gmail.com>
2025-05-15 00:41:25 -0400

    LC1148

---
+++ CreateTables/lc1148Table.sql
@@ -0,0 +1,8 @@
+-- LC1148. Article Views I
+-- this table doesn't have primary keys
+
+create table Views
+(article_id int,
+author_id int,
+viewer_id int,
+view_date date);
---
+++ Query/lc1148Query.sql
@@ -0,0 +1,8 @@
+-- =LC1148. Article Views I
+
+-- Write a solution to find all the authors that viewed
+-- at least one of their own articles.
+-- Return the result table sorted by id in ascending order.
+
+select distinct (author_id) as id from views
+where author_id = viewer_id;
